bf1df7b54e0baa4a1e375b4657b663110a819fa3
KirillDolzhenko <manfeed3@gmail.com>
2024-10-18 23:50:42 +0300

    Vite configures added

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This is the first time I'm trying to write something on Tailwind, so don't judge
 
 # Summary
 
-So there is I tested a bunch of new features for me like CSS-animations, Tailwind, Prettier configurations and even writting README file for GitHub repo)
+So there is I tested a bunch of new features for me like CSS-animations, Tailwind, Prettier configurations, Vite, Husky, Eslint and even writting README file for GitHub repo)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import dotenv from "dotenv";
 
-// https://vitejs.dev/config/
+const config = dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: Number(config.parsed?.VITE_PORT_SERVER) || 4000
+  },
+  preview: {
+    port: Number(config.parsed?.VITE_PORT_PREVIEW) || 5000
+  }
 });
